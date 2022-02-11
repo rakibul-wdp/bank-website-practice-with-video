f02a531b986e6d81618616e68e040d1fe23f3097
rakibul-wdp <rakibul.wdp@gmail.com>
2022-02-11 17:51:05 +0600

    deposit calculatio

--- js/banking.js
+++ js/banking.js
@@ -51,3 +51,17 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     // clear withdraw input
     withdrawInput.value = '';
 })
+
+// again banking.html page calculation by javascript with module 27::
+
+document.getElementById("deposit-button").addEventListener("click", function () {
+    const depositInput = document.getElementById("deposit-input");
+    const depositAmount = depositInput.value;
+    // get current deposit
+    const depositTotal = document.getElementById("deposit-total");
+    const depositTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositAmount;
+    console.log(depositTotalText);
+    // clear input field
+    depositInput.value = "";
+});
